1e214555b8b86187686f1acd4ca70c895f9faaf6
Ritesh102000 <ritesh23075@iiitd.ac.in>
2024-04-20 23:38:08 +0530

    Intigration of Frontend done with llm modal

---
+++ FrontEndIntigration/chatbot/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from . import llm
+def chatbot(request):
+    #chats = Chat.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        #response = ask_openai(message)
+        response = llm.ask_Query(message)
+        #chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        #chat.save()
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html')
