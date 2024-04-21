4dff8773ffba669f21e8c94ab4971ba3c87b3d6e
vanshaj1 <vanshajsharma0612@gmail.com>
2024-04-21 15:52:40 +0530

    User Feedback partially created by Vanshaj and Pulkit

--- FrontEndIntigration/chatbot/llm.py
+++ FrontEndIntigration/chatbot/llm.py
@@ -1,4 +1,5 @@
 
+#******************************Libraries**********************************
 import pandas as pd
 import pandas as pd
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -8,13 +9,19 @@ from langchain.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
 # from langchain.chains import RetrievalQA
 from langchain import PromptTemplate
-
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import create_retrieval_chain
+from rank_bm25 import BM25Okapi
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import (
     ChatGoogleGenerativeAI,
     HarmBlockThreshold,
     HarmCategory,
 )
+
+#******************************** LLM MODEL ******************************************
 Gemini_pro_llm = ChatGoogleGenerativeAI(model="gemini-pro",
                                         google_api_key="AIzaSyBSaMUOmN4usfGXD9PT7OmDcCFCa9WvBMc",
                                         temperature=0.0,
@@ -26,14 +33,18 @@ Gemini_pro_llm = ChatGoogleGenerativeAI(model="gemini-pro",
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
     })
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+#******************************** LLM Embeddings *****************************
 GeminiEmbeddingModel = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key="AIzaSyBSaMUOmN4usfGXD9PT7OmDcCFCa9WvBMc")
+
+#******************************** Preparing data ********************************
 df = pd.read_csv('football_data_with_summary_V2.0.csv')
 headingsList = df["content"].tolist()
 tokenized_corpus = [doc.split(" ") for doc in headingsList]
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+bm25 = BM25Okapi(tokenized_corpus)
+# Explain about the following Topic in depth using the provided context and your knowledge
+#****************************** Prompt creation **********************************
+prompt = ChatPromptTemplate.from_template("""Explain about the following Question in depth:
 
     <context>
     {context}
@@ -41,20 +52,23 @@ prompt = ChatPromptTemplate.from_template("""Answer the following question based
 
     Question: {input}""")
 
+
+#**************************** Chaining Prompt and LLM ******************************
 document_retrieval_chain = create_stuff_documents_chain(Gemini_pro_llm, prompt)
-from langchain.chains import create_retrieval_chain
-from rank_bm25 import BM25Okapi
-import gradio as gr
-# # "What are the Requirements for M.tech Specialization. ?"
-bm25 = BM25Okapi(tokenized_corpus)
+
+
+
 def ask_Query(query):
     tokenized_query = query.split(" ")
-    doc_scores = bm25.get_scores(tokenized_query)
+    # doc_scores = bm25.get_scores(tokenized_query)
 
     a = bm25.get_top_n(tokenized_query, headingsList, n=2)
     # a[0] = "Sport as a Symbol of National Power" # hardcoded
     # resultContent = df.loc[df['Summary'] == a[0], 'content'].iloc[0]
-    resultContent = a[0] + '. '+ a[1]
+    doc = ""
+    for i in range(2):
+        doc = doc + a[i] + '. '
+    resultContent = doc
 
     textSplitter = RecursiveCharacterTextSplitter()
     text = textSplitter.split_text(resultContent)
@@ -67,6 +81,7 @@ def ask_Query(query):
     Question_Answer_Chain = create_retrieval_chain(retriever, document_retrieval_chain)
 
     result = Question_Answer_Chain.invoke({"input": query})
+
     return result["answer"]
 
 
