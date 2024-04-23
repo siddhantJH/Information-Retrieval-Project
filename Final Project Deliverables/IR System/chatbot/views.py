fc479ce9d6513e18e05c54e560e0f89646d45fbf
Pulkit <pulkit@Pulkits-MacBook-Air.local>
2024-04-23 23:12:58 +0530

    Error handling

--- Final Project Deliverables/IR System/chatbot/views.py
+++ Final Project Deliverables/IR System/chatbot/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 import markdown
 from . import llm
 from . import paraphraser
@@ -17,7 +17,13 @@ def chatbot(request):
 def paraphrase(request):
     if request.method == 'POST':
         para_phrases = paraphraser.parrot.augment(input_phrase=request.POST.get('message'))
-        print(para_phrases)
-        response = para_phrases[random.randint(0,len(para_phrases)-1)][0]
+        index=0
+        if(para_phrases!=None):
+            index = random.randint(0,len(para_phrases)-1)
+        else:
+            index=0
+        response = para_phrases[index][0]
+        if(response == None):
+            return HttpResponse('<h1>Page not found</h1>', status_code='404')
         return JsonResponse({'response': response})
     return render(request, 'chatbot.html')
